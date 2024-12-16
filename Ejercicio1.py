36cbc84c027342b418c61b6de6c861a3c075488b
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-12-16 11:25:25 +0100

    Ejercicio 1
    Subo ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,7 @@
+numero = int(input("Introduce el número entero del 1 al 10 para devolver su tabla de multiplicar: "))
+tablamult = ""
+for i in range(1, 11):
+    tablamult += str(numero) + " * " + str(i) + " = " + str(numero * i) + "\n"
+nombre = "tabla-" + str(numero) + ".txt"
+file = open(nombre, 'w')
+file.write(tablamult)
