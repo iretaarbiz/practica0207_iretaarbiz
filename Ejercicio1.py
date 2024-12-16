406918a061a59345f008d54de99c4489aa152e87
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-12-16 11:30:47 +0100

    Ejercicio 1
    Corrección ejercicio 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,7 +1,8 @@
-numero = int(input("Introduce el número entero del 1 al 10 para devolver su tabla de multiplicar: "))
+numero = int(input("Introduce el número entero del 1 al 10 para guardar su tabla de multiplicar: "))
 tablamult = ""
 for i in range(1, 11):
     tablamult += str(numero) + " * " + str(i) + " = " + str(numero * i) + "\n"
 nombre = "tabla-" + str(numero) + ".txt"
 file = open(nombre, 'w')
 file.write(tablamult)
+file.close()
