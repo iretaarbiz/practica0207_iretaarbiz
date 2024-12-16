e349fdc0d16d796be96a9ba2be345a8b054c01b1
iretaarbiz <iretaarbiz@educacion.navarra.es>
2024-12-16 11:38:16 +0100

    Ejercicio 2
    Subo ejercicio 2

---
+++ Ejercicio2.py
@@ -0,0 +1,8 @@
+import os
+numero = int(input("Introduce el número entero del 1 al 10 para devolver su tabla de multiplicar: "))
+nombre = "tabla-" + str(numero) + ".txt"
+if os.path.isfile(nombre):
+    file = open(nombre, 'r')
+    print(file.read())
+else:
+    print("El fichero no existe")
